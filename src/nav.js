5ec7c6a95e9a26bad2a64459545fddedb051bc0c
Arjun8124 <gupta.arjun.8124@gmail.com>
2025-10-15 15:12:24 +0530

    made a basic navbar and added a button to add jobs

---
+++ src/nav.js
@@ -0,0 +1,9 @@
+import "./App.css";
+export default function Nav() {
+  return (
+    <div className="navbar">
+      <h2 className="icon">💼Job Tracker🏢</h2>
+      <button className="addJob">➕Add New Job</button>
+    </div>
+  );
+}
